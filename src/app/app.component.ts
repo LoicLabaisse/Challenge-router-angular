b6c516bc5609a9d4fce9b9e0cfb75e36474f9365
LoicLabaisse <69313495+LoicLabaisse@users.noreply.github.com>
2021-07-07 12:06:12 +0200

    add chapter 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myFirstApp';
+  title = 'Project_Angular';
+  isThisIngredientVital : boolean = true;
+  displayGuestList: boolean = false;
+  songList:string[]= ["Alejandro", "Ne me quitte pas","Le temps est bon"];
 }
